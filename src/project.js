cb4c142c93c3c52bb6851adc186396a2c69dacd4
mke2111 <roymke134@gmail.com>
2021-01-28 19:59:07 +0300

    add todo and project classes

---
+++ src/project.js
@@ -0,0 +1,5 @@
+class Project {
+  constructor (name) {
+    this.name = name;
+  }
+}
---
+++ src/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+  constructor (title, description, dueDate, priority, checked) {
+    this.tittle = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checked = checked;
+  }
+}
+
+export default Todo;
