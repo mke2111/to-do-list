a8aa687bcddadeb741f75fb3d5e788b6aaf47ac5
mke2111 <roymke134@gmail.com>
2021-01-28 21:20:11 +0300

    Add buttons

--- src/heading.js
+++ src/heading.js
@@ -1,12 +1,27 @@
 const heading = () => {
   const headDiv = document.createElement('div');
-  headDiv.classList.add('text-center', 'text-4xl');
+  headDiv.classList.add('text-center');
 
   const headContent = document.createElement('h2');
-  headContent.classList.add('text-red-700');
+  headContent.classList.add('text-red-700', 'text-4xl');
   headContent.innerText = 'Your daily To-Do list';
 
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('flex', 'flex-row', 'justify-around', 'mx-40', 'pt-6');
+
+  const todoBtn = document.createElement('button');
+  todoBtn.classList.add('bg-green-400', 'px-5', 'py-2', 'rounded-full');
+  todoBtn.innerText = 'New To-Do';
+
+  const projectBtn = document.createElement('button');
+  projectBtn.classList.add('bg-green-400', 'px-5', 'py-2', 'rounded-full');
+  projectBtn.innerText = 'New Project';
+
+  btnDiv.appendChild(todoBtn);
+  btnDiv.appendChild(projectBtn);
+
   headDiv.appendChild(headContent);
+  headDiv.appendChild(btnDiv);
 
   return headDiv;
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import heading from './heading';
 const content = document.querySelector('#content');
 content.classList.add('bg-red-50', 'h-screen', 'pt-6');
 
+
+
+
 content.appendChild(heading());
 
